4a03ab07ddf91ac588e5b16daabc9af601ddb46a
Romina D-R <rdr2711@yahoo.com>
2018-06-22 14:07:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file.
+const studentNames = ['Adele', 'Beyoncé', 'Lady', 'Madonna', 'Rihanna', 'Taylor']
+
+function lowerCaseStudentNames(list) {
+  return list.map(hi);
+  
+}  
+ 
+function hi(student) {
+  return student.toLowerCase();
+}
+
+function splitName(name) {
+  const studentFirst = name.split(" ")[0];
+  const studentLast = name.split(" ")[1];
+  
+  return { firstName : studentFirst, lastName : studentLast}
+}
+
+function nameToAttributes(list) {
+  return list.map(splitName)
+}
+
+function identifyFromObjects(object) {
+  const name = object.name
+  const hometown = object.hometown
+  
+  return `${name} is from ${hometown}`
+}
+
+function attributesToPhrase(list) {
+  return list.map(identifyFromObjects)
+}
+
+
+
+
